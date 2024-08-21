98ae046960359830451f85c4badcb5014b015dc2
Anuja-Aliveli <a.anuja54@gmail.com>
2024-08-21 19:40:17 +0530

    Created components tasklist, taskview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { TaskFormComponent } from './TaskForm/TaskForm.component';
+import { TaskListComponent } from './TaskList/TaskList.component';
+import { TaskViewComponent } from './TaskView/TaskView.component';
 
 export const routes: Routes = [
-    
+
   { path: 'tasks/create', component: TaskFormComponent },
-  
+  { path: 'tasks/edit', component: TaskFormComponent },
+  { path: 'tasks/list', component: TaskListComponent },
+  { path: 'tasks/view', component: TaskViewComponent },
 ];
