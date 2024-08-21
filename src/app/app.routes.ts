4d7d0bc825dcd8cfefd269ef1cfba64c2fcb8bef
Anuja-Aliveli <a.anuja54@gmail.com>
2024-08-21 15:42:28 +0530

    task form component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TaskFormComponent } from './TaskForm/TaskForm.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+  { path: 'tasks/create', component: TaskFormComponent },
+  
+];
